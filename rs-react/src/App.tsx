4825367da4ba0ab64360bb06d9b7c1d18a6fe095
Vladimir Kurganov <err0rfrost@gmail.com>
2023-03-16 19:21:34 +0600

    feat: add router

--- rs-react/src/App.tsx
+++ rs-react/src/App.tsx
@@ -1,5 +1,29 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/404';
+
+// layouts
+import RootLayout from './layouts/RootLayout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />} errorElement={<NotFound />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
+
 function App() {
-  return <div>Hello world!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
