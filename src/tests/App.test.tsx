27edafa003f8934572cd56a1acd79a41fb5a1e46
Vladimir Kurganov <err0rfrost@gmail.com>
2023-03-20 02:33:23 +0600

    feat: add Card tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-describe('App render', () => {
+describe('App Home render', () => {
   it('it should render App', () => {
     render(<App />);
     expect(screen.getByText(/RSS - React/i)).toBeInTheDocument();
--- src/tests/Products.test.tsx
+++ src/tests/Products.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { ProductList } from '../components/productsWrapper';
+import ProductCard from '../components/product';
 import products from './mock';
 
 describe('Render ProductCards', () => {
@@ -8,5 +9,17 @@ describe('Render ProductCards', () => {
     const productCards = screen.getAllByTestId('product-card');
     expect(productCards[0]).toBeInTheDocument();
     expect(productCards.length).toBe(8);
+  });
+});
+
+describe('Render Card', () => {
+  it('it should render Card details', () => {
+    const product = products[0];
+    render(<ProductCard key={product.id} product={product} />);
+    expect(screen.getByText(/iPhone 9/i)).toBeInTheDocument();
+    expect(screen.getByText(/549/i)).toBeInTheDocument();
+    expect(screen.getByText(/Smartphones/i)).toBeInTheDocument();
+    expect(screen.getByText(/Apple/i)).toBeInTheDocument();
+    expect(screen.getByText(/4.69/i)).toBeInTheDocument();
   });
 });
