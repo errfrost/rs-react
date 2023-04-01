ca4b47a2a232563e56e61a5f27816b274237e96d
Vladimir Kurganov <err0rfrost@gmail.com>
2023-04-02 00:09:18 +0700

    feat: add FormCards test

--- src/tests/form.test.tsx
+++ src/tests/form.test.tsx
@@ -1,9 +1,22 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Form from '../components/form';
+import { FormCards } from '../components/formCardsWrapper';
 
 describe('Render Form', () => {
   it('it should render Form', () => {
-    const link = render(<Form newCard={() => {}} />);
+    const link = render(<Form newCard={() => { }} />);
     expect(link).toBeTruthy();
+  });
+});
+
+describe('Render FormCards', () => {
+  it('it should render FormCards', () => {
+    const card = { firstName: "Joe", lastName: "Biden", birthday: "12-12-2000", country: "USA", gender: "Male", confirmData: true, photo: " " };
+    const link = render(<FormCards cards={[card]} />);
+    expect(screen.getByText(/Joe/i)).toBeInTheDocument();
+    expect(screen.getByText(/Biden/i)).toBeInTheDocument();
+    expect(screen.getByText(/12-12-2000/i)).toBeInTheDocument();
+    expect(screen.getByText(/USA/i)).toBeInTheDocument();
+    expect(screen.getByText(/Male/i)).toBeInTheDocument();
   });
 });
